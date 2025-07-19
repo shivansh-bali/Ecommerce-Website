e9f2596fd9c8f122c0cb7e3682ad1b82f518942f
shivansh-bali <bali.shivaansh@gmail.com>
2025-07-19 21:06:05 +0530

    Add files via upload

---
+++ Backend/dist/src/app.module.js
@@ -0,0 +1,42 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const mongoose_1 = require("@nestjs/mongoose");
+const app_controller_1 = require("./app.controller");
+const app_service_1 = require("./app.service");
+const users_module_1 = require("./users/users.module");
+const products_module_1 = require("./products/products.module");
+const orders_module_1 = require("./orders/orders.module");
+const customers_module_1 = require("./customers/customers.module");
+const markets_module_1 = require("./markets/markets.module");
+const analytics_module_1 = require("./analytics/analytics.module");
+const content_module_1 = require("./content/content.module");
+const discounts_module_1 = require("./discounts/discounts.module");
+const marketing_module_1 = require("./marketing/marketing.module");
+let AppModule = class AppModule {
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [users_module_1.UsersModule,
+            products_module_1.ProductsModule,
+            orders_module_1.OrdersModule,
+            customers_module_1.CustomersModule,
+            markets_module_1.MarketsModule,
+            analytics_module_1.AnalyticsModule,
+            content_module_1.ContentModule,
+            discounts_module_1.DiscountsModule,
+            marketing_module_1.MarketingModule,
+            mongoose_1.MongooseModule.forRoot('mongodb://localhost/nest')],
+        controllers: [app_controller_1.AppController],
+        providers: [app_service_1.AppService],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
